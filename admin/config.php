52ba26b9cc513b9a93d4fa69832fd89106b5d5e9
sergey <sergey_kariy_55@mail.ru>
2017-03-01 09:47:15 +0300

    new

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://battery.ch41700.tmweb.ru/admin/');
+define('HTTP_CATALOG', 'http://battery.ch41700.tmweb.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://battery.ch41700.tmweb.ru/admin/');
+define('HTTPS_CATALOG', 'http://battery.ch41700.tmweb.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/home/c/ch41700/battery/public_html/admin/');
+define('DIR_SYSTEM', '/home/c/ch41700/battery/public_html/system/');
+define('DIR_IMAGE', '/home/c/ch41700/battery/public_html/image/');
+define('DIR_LANGUAGE', '/home/c/ch41700/battery/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/c/ch41700/battery/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/c/ch41700/battery/public_html/system/config/');
+define('DIR_CACHE', '/home/c/ch41700/battery/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/c/ch41700/battery/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/c/ch41700/battery/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/c/ch41700/battery/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/c/ch41700/battery/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/home/c/ch41700/battery/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'ch41700_battery');
+define('DB_PASSWORD', '621968592');
+define('DB_DATABASE', 'ch41700_battery');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://battery.ch41700.tmweb.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://battery.ch41700.tmweb.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/home/c/ch41700/battery/public_html/catalog/');
+define('DIR_SYSTEM', '/home/c/ch41700/battery/public_html/system/');
+define('DIR_IMAGE', '/home/c/ch41700/battery/public_html/image/');
+define('DIR_LANGUAGE', '/home/c/ch41700/battery/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/c/ch41700/battery/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/c/ch41700/battery/public_html/system/config/');
+define('DIR_CACHE', '/home/c/ch41700/battery/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/c/ch41700/battery/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/c/ch41700/battery/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/c/ch41700/battery/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/c/ch41700/battery/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'ch41700_battery');
+define('DB_PASSWORD', '621968592');
+define('DB_DATABASE', 'ch41700_battery');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
